90d873114663303fbcb76b5d222088cbc5078f92
Pranit <govandepranit@gmail.com>
2023-11-29 15:37:15 +0530

    added files

---
+++ script.js
@@ -0,0 +1,13 @@
+function goToMovieDetails(movieName) {
+  // You can navigate to the second page here
+  window.location.href = `movieDetails.html?movie=${movieName}`;
+}
+// Function to navigate to the second page
+function goToMovieDetails(movieName) {
+  window.location.href = `movieDetails.html?movie=${movieName}`;
+}
+
+// Function to navigate to the third page
+function goToTicketConfirmation(theaterName) {
+  window.location.href = `ticketConfirmation.html?theater=${theaterName}`;
+}
